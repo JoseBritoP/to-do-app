702a2e07051cca59fd6eb0496c9a3e938f5676b8
José Brito <josealbritose@gmail.com>
2023-12-07 13:36:25 -0300

    Modularizado

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useTodos } from "./hooks/useTodoHook";
 export default function App() {
 
   const { filterSelected, activeCount, completedCount, filteredTodos, handleFilterChange, handleRemove, handleRemoveAllCompleted, handleCompleted, handleAddTodo } = useTodos();
+  
   return (
     <div className="todoapp">
       <Header onAddTodo={handleAddTodo}/>
